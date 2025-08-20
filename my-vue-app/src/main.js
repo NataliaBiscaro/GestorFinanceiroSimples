182f6402a29e2e714d20053463a3068f10cbe1ad
nati <biscaro321@gmail.com>
2025-08-20 19:38:55 -0300

    GF-01 : Adding Quasar to the project

--- my-vue-app/src/main.js
+++ my-vue-app/src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
 import './style.css'
 import App from './App.vue'
+import 'quasar/src/css/index.sass'
+import '@quasar/extras/material-icons/material-icons.css'
 
-createApp(App).mount('#app')
+
+createApp(App)
+    .use(Quasar, {
+        plugins: {} // se precisarmos de Dialog/Notify etc., ativamos aqui depois
+    })
+    .mount('#app')
